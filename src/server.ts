f6b37d6463a4e81a32e6a48da570069cbc793bac
starshpping333 <starshopping3333@gmail.com>
2025-08-08 21:06:07 -0300

    ajustes

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app"
+import { AppDataSource } from "./data-source"
+
+AppDataSource.initialize().then(()=>{
+    console.log("database conectado")
+     app.listen(3001,()=>{
+        console.log("server is running")
+     })
+}).catch((error)=>{
+    console.log(error)
+})
+
+
