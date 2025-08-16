945a7d6f051601be47924f8128d38a58d69d0450
starshpping333 <starshopping3333@gmail.com>
2025-08-15 21:25:16 -0300

    get put config

--- src/Routes/Admin.Routes.ts
+++ src/Routes/Admin.Routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { ValidateDataMiddleware } from "../Middleware/ValidateData.Middleware";
-import { CreateAdminConfigSchemas, ReturnConfigAdminSchemas } from "../Schemas/AdminConfig.Schemas";
-import { ConfigAdmController, PutAdmConfig } from "../Controllers/Admin.Controller";
+import { CreateAdminConfigSchemas, ReturnConfigAdminSchemas, UpdateAdminConfigSchema } from "../Schemas/AdminConfig.Schemas";
+import { ConfigAdmController, GetConfigController, PutAdmConfig } from "../Controllers/Admin.Controller";
+import { validateTokenmiddleware } from "../Middleware/ValidateToken.Middleware";
 
 export const AdminRoutes:Router = Router()
 
-AdminRoutes.post("",ValidateDataMiddleware(CreateAdminConfigSchemas),ConfigAdmController)
-AdminRoutes.put("",ValidateDataMiddleware(ReturnConfigAdminSchemas),PutAdmConfig)
+AdminRoutes.post("",ValidateDataMiddleware(CreateAdminConfigSchemas),validateTokenmiddleware,ConfigAdmController)
+AdminRoutes.put("",ValidateDataMiddleware(UpdateAdminConfigSchema),validateTokenmiddleware,PutAdmConfig)
+AdminRoutes.get("",GetConfigController)
