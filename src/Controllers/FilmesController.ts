39e34d65fde0c4f6ac10512d6e513372e2acaa44
Eduardofborges55 <eduardofborges55@gmail.com>
2025-08-27 21:01:11 -0300

    fix: ajuste do login

---
+++ src/Controllers/FilmesController.ts
@@ -0,0 +1,37 @@
+import e, { Request, Response } from "express";
+
+import { FavoritarFilmesService } from "../Service/CreateFavoritarFilmes.Service";
+import { iCreateFavoritar } from "../Schemas/FavoritarFilmes.Schemas";
+import { getFilmesService } from "../Service/getfilmes.service";
+import { GetAllFilmesFavService } from "../Service/GetAllFilmesFav.Service";
+import { DeleteFilmeFavService } from "../Service/DeleteFilmeFav.Service";
+
+
+export const getFilmesController= async(req:Request,res:Response):Promise<Response>=>{
+
+        const filmes = await getFilmesService()
+        return res.status(200).json(filmes)
+}
+
+export const FavoritarFilmesController = async(req:Request,res:Response)=>{
+
+        const favData:iCreateFavoritar = req.body
+      
+        const filme = await FavoritarFilmesService(favData)
+        return res.status(200).json(filme)
+        
+}
+
+export const GetFilmesFavController = async (req:Request,res:Response):Promise<Response>=>{
+        const userId= req.User?.id
+        const id = Number(userId)
+        const FavFilmes = await GetAllFilmesFavService(id)
+        return res.status(200).json(FavFilmes)
+}
+
+export const DeleteFilmeFavController = async (req:Request,res:Response):Promise<Response>=>{
+        const favoritoID = parseInt(req.params.id)
+        
+        await DeleteFilmeFavService(favoritoID)
+        return res.status(204).send()
+}
--- src/Service/CreateLogin.Service.ts
+++ src/Service/CreateLogin.Service.ts
@@ -4,8 +4,8 @@ import { AppDataSource } from "../data-source";
 import { AppError } from "../error";
 import jwt from "jsonwebtoken"
 import {compare} from "bcryptjs"
-import { iCreateLogin } from "../Schemas/Login.Schema";
-export const createLoginService=async(loginData:iCreateLogin):Promise<string>=>{
+import { iCreateLogin, iRetunrLogin, returnLoginSchema } from "../Schemas/Login.Schema";
+export const createLoginService=async(loginData:iCreateLogin):Promise<iRetunrLogin>=>{
 
     const userRepository:Repository<Usuarios> = AppDataSource.getRepository(Usuarios)
 
@@ -33,6 +33,11 @@ export const createLoginService=async(loginData:iCreateLogin):Promise<string>=>{
             subject:String(findUser.id)
         }
     )
-        return token
+
+    const user = returnLoginSchema.parse({
+         token,
+         usuario:findUser
+    })
+        return user
     
 }
